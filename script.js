9e3f9fdadd438c8ccb6330112384a94945da40a2
Tanmay Deshmukh <99901740+qwertytanmay@users.noreply.github.com>
2023-09-16 22:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
 
+	//get the form element
+	let myForm = document.getElementsByID("form1");
+	//get the input values
+	let firstname = myForm.elements.fname.value;
+	let lastname = myForm.elements.lname.value;
+	//alert
+	alert(firstname+" "+lastname);
 }
